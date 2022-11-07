49efa395a3d289de954fdb1d305dd243a62208c6
Raymundo Valdez <ing.ray.valdez@gmail.com>
2022-11-07 00:35:21 -0600

    Initial commit.

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,31 @@
+public class Vehicle {
+
+    private int milesPerGallon = 20;
+    private double gallonsOfGas = 10.0;
+    private int odometer = 0;
+
+
+    public int getMilesPerGallon() {
+        return milesPerGallon;
+    }
+
+    public void setMilesPerGallon(int milesPerGallon) {
+        this.milesPerGallon = milesPerGallon;
+    }
+
+    public double getGallonsOfGas() {
+        return gallonsOfGas;
+    }
+
+    public void setGallonsOfGas(double gallonsOfGas) {
+        this.gallonsOfGas = gallonsOfGas;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+}
